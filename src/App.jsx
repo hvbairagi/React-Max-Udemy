68dcfe6ef7460d01c5d787142da5672c626da249
Harsh Vardhan Bairagi <54072364+hvbairagi@users.noreply.github.com>
2024-06-10 22:25:38 +0530

    project init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import AuthInputs from './components/AuthInputs.jsx';
+import Header from './components/Header.jsx';
 
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
 }
-
-export default App;
